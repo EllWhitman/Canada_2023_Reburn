6889b0d2f517bb8964e0c25ba1b9b52173d02707
EllWhitman <59099392+EllWhitman@users.noreply.github.com>
2025-02-06 09:37:12 -0800

    Update 2.Convert_Reburn_Rasters_ToPolys.R

--- R/2.Convert_Reburn_Rasters_ToPolys.R
+++ R/2.Convert_Reburn_Rasters_ToPolys.R
@@ -10,6 +10,7 @@ setwd(#Your path here
   "/Data/Yearly_Reburned_Area/Tiffs")
 rasts = list.files(pattern = "\\.tif$") #List yearly reburn rasters
 yrs = seq(1982,2023,by=1)#List of years for analysis
+window = 20 #Change this to whatever the reburn duration window used for analysis in script 1 was
 for(i in 1:length(rasts)){
   rb.ras = read_stars(rasts[i])#Read in
   rb.p2 = st_as_sf(rb.ras,as_points = FALSE,merge=T)#Convert to Poly using st
@@ -17,7 +18,7 @@ for(i in 1:length(rasts)){
   rm(rb.ras)
   gc()
   st_write(rb.p2, paste(#Your path here
-    "/Data/Yearly_Reburned_Area/Polys/Reburns_10_",yrs[i],".shp",sep=""), delete_layer = TRUE)
+    "/Data/Yearly_Reburned_Area/Polys/Reburns_" window,"_",yrs[i],".shp",sep=""), delete_layer = TRUE)
   print(paste("stars Polygonized Year ",yrs[i],sep=""))
   i=i+1
 }
@@ -31,6 +32,7 @@ polys = list.files(pattern = "\\.shp$") #List yearly reburn polys
 #This is inefficent, sorry, but we are now going to change this to terra polygons so that it is easier to calculate area, 
 #and to make them separate patches for each year, and combine into a single polygon of the time series
 yrs = seq(1982,2023,by=1)
+window = 20 #Change this to whatever the reburn duration window used for analysis in script 1 was
 for(j in 1:length(polys)){
   rb.pol = vect(polys[j])#Read in
   rb.pol = disagg(rb.pol)
@@ -48,6 +50,6 @@ head(reburns)
 
 writeVector(reburns, 
             #Your path here
-            "/Data/Reburns_TimeSeries_Complete_19802023_10yr.shp", overwrite = TRUE)
+            paste("/Data/Reburns_TimeSeries_Complete_19822023_",window,"yr.shp",sep=""), overwrite = TRUE)
 rm(list=ls())
 gc()
