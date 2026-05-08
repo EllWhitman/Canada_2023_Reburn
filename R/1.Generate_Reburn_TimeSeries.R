d5f3b02c6201a6c02c84760e68827b1b83534d5d
EllWhitman <59099392+EllWhitman@users.noreply.github.com>
2026-05-08 11:14:50 -0600

    Update 1.Generate_Reburn_TimeSeries.R
    typo

--- R/1.Generate_Reburn_TimeSeries.R
+++ R/1.Generate_Reburn_TimeSeries.R
@@ -6,7 +6,7 @@ library(terra)
 #Written by Ellen Whitman
 #ellen.whitman@nrcan-rncan.gc.ca
 #Last Edit Nov 25, 2025
-#Wherever 'Your path here' appears as a comment, please replace with the correct pathway to your local file
+#Whereever 'Your path here' appears as a comment, please replace with the correct pathway to your local file
 #The ecozones file can be downloaded here: https://sis.agr.gc.ca/cansis/nsdb/ecostrat/gis_data.html
 #The NBAC fire perimeter maps can be downloaded here:https://cwfis.cfs.nrcan.gc.ca/datamart
 #If the path above ceases to function, access the NBAC maps via the main CWFIS page: https://cwfis.cfs.nrcan.gc.ca/
